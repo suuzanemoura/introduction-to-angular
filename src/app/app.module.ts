a7fcff144edcf083d46723cff7a08dd6b8383c11
suuzanemoura <104701271+suuzanemoura@users.noreply.github.com>
2023-09-06 10:53:50 -0300

    :sparkles: feat: primeiros components em angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, BrowserModule],
+  declarations: [],
+  bootstrap: [HeaderComponent, ContentComponent, FooterComponent],
 })
-export class AppModule { }
+export class AppModule {}
